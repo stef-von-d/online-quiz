1f40d2524474867faab2af9ee6bb60a06102eae3
stef-von-d <stefania.pastramagiu@yahoo.com>
2024-05-11 12:53:21 +0000

    Add questions for quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,70 @@
-console.log('Connected!')
+const questions = [
+    {
+        questions: 'Who is the capital of Romania?',
+        options: ['Bucharest', 'Paris', 'Rome', 'Madrid'],
+        answer: 'Bucharest',
+    },
+    {
+        question: 'What is the most spoken language in the world?',
+        options: ['Spanish', 'Spanish', 'Italian', 'Chinese'],
+        answer: 'Spanish',
+    },
+    {
+        question: 'What is the tallest animal in the world?',
+        options: ['Whales', 'Elephant', 'Giraffes', 'Dogs'],
+        answer: 'Giraffes',
+    },
+    {
+        question: "What is the biggest continent?",
+        options: ['Europe', 'Asia', 'North America', 'Africa'],
+        answer: 'Asia',
+    },
+    {
+        question: 'What is the capital of Canada?',
+        options: ['Ottawa', 'London', 'Montreal', 'Vancouver'],
+        answer: 'Ottawa',
+    },
+
+    {
+        question: 'What is the best-selling book series of the 21st century?',
+        options: ['Harry Potter', 'Hunger Games', 'Notebook', 'Twilight'],
+        answer: 'Harry Potter',
+    },
+
+    {
+        question: 'When did London underground open?',
+        options: ['1900', '1903', '1905', '1863'],
+        answer: '1863',
+    },
+
+    {
+        question: 'What city do the Beatles come from?',
+        options: ['Brighton', 'London', 'Manchester', 'Liverpool'],
+        answer: 'Liverpool',
+    },
+
+    {
+        question: 'How many days does it take for Earth to orbit the Sun?',
+        option: ['360', '362', '364', '365'],
+        answer: '365'
+    },
+
+    {
+        question: 'What language is spoken in Brazil?',
+        option: ['Spanish', 'Brazilian', 'Portuguese', 'English'],
+        answer: 'Portuguese',
+    },
+
+    {
+        question: 'What is the main ingredient in hummus?',
+        option: ['Chickpeas', 'Lentils', 'Potatoes', 'Beans'],
+        answer: 'Chickpeas' ,
+    },
+
+    {
+        question: 'The human adult have ... teeth?',
+        option: ['33', '32', '34', '31'],
+        answer: '32',
+    },
+
+];
