16ec771f8f02e8cf4cfbb1544ab45acc20df530d
stef-von-d <stefania.pastramagiu@yahoo.com>
2024-05-14 06:22:27 +0000

    start quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,88 @@
 const questions = [
-    {
-        question: "Who is the of Romania? " ,
-        answers: [
-            {text:"Bucharest" , correct: true},
-            {text:"Rome" , correct: false} , 
-            {text: "Madrid" , correct: false},
-            {text: "London" , correct: false},
-        ]
-    }, 
+  {
+    question: "Who is the of Romania? ",
+    answers: [
+      { text: "Bucharest", correct: true },
+      { text: "Rome", correct: false },
+      { text: "Madrid", correct: false },
+      { text: "London", correct: false },
+    ]
+  },
 
-    {
-        question: "What is the most spoken language in the world?" ,
-        answers: [
-        {text: "English" , correct: false}, 
-        {text:"Italian" , correct: false},
-        {text:"Chinese" , correct: false}, 
-        {text:"Spanish" , correct:true} 
-        ]
-    },
+  {
+    question: "What is the most spoken language in the world?",
+    answers: [
+      { text: "English", correct: false },
+      { text: "Italian", correct: false },
+      { text: "Chinese", correct: false },
+      { text: "Spanish", correct: true }
+    ]
+  },
 
-    {
-        question: "What is the biggest continent? " ,
-        answers: [
-            {text: "Europe", correct: false} ,
-            {text: "Africa", correct: false} ,
-            {text:"Asia" correct: true} ,
-            {text:"North America" , correct: false},
-        ]
-    }, 
+  {
+    question: "What is the biggest continent? ",
+    answers: [
+      { text: "Europe", correct: false },
+      { text: "Africa", correct: false },
+      { text: "Asia", correct: true },
+      { text: "North America", correct: false },
+    ]
+  },
 
-    {
-        question: "How many times zones are in Russia?" ,
-        answers: [
-            {text: "3" , correct: false},
-            {text: "8", correct: false}, 
-            {text:"11" , correct: true}, 
-            {text:"2", correct: false},
-        ]
-    }, 
+  {
+    question: "How many times zones are in Russia?",
+    answers: [
+      { text: "3", correct: false },
+      { text: "8", correct: false },
+      { text: "11", correct: true },
+      { text: "2", correct: false },
+    ]
+  },
 
-    {
-        question:"What is the national flower of Japan?" , 
-        answers: [
-            {text: "Sunflower" , correct: false}, 
-            {text: "Cherry Blossom", correct: true},
-            {text: "Aloe Vera" , correct: false},
-            {text:"None", correct: false} , 
-        ],
-    }
-   
+  {
+    question: "What is the national flower of Japan?",
+    answers: [
+      { text: "Sunflower", correct: false },
+      { text: "Cherry Blossom", correct: true },
+      { text: "Aloe Vera", correct: false },
+      { text: "None", correct: false },
+    ],
+  }
+];
 
 const questionElement = document.getElementById('question');
-const answerButton = document.getElementById('ans_btn');
+const answerButton = document.getElementById('answer_buttons');
 const nextButton = document.getElementById('next_button');
 
 let currentQuestionIndex = 0;
 let score = 0;
 
 function startQuiz() {
-    currentQuestionIndex = 0;
-    score = 0;
-    nextButton.innerHTML = "Next";
-    showQuestion();
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
 }
 
-function showQuestion(){
-    let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+function showQuestion() {
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = " " + currentQuestion.question
+  console.log(currentQuestion);
+  console.log(currentQuestion.question);
+
+
+  const button = document.createElement("button");
+  button.innerHTML = currentQuestion.answer;
+
+  button.classList.add("btn");
+  answerButton.appendChild(button);
+
 }
+
+startQuiz();
+
+
+
+
+
